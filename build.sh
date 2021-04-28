d4bce912662c499a3186cb49b6dcf628bb2e6912
AndrewChen <chenpoanandrew@gmail.com>
2021-04-28 17:05:38 +0800

    fix: fix build process

--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@
 set -e
 
 TAG=$(git tag --points-at HEAD)
-IMAGE_NAME=andrewchen20/protoc-golang:$TAG
+IMAGE_NAME=andrewchen20/protoc_golang:$TAG
 
-docker build . 
-
-docker tag mockingbird:latest "$IMAGE_NAME"
+docker build -t "$IMAGE_NAME" .
 
 sudo docker push "$IMAGE_NAME"
